86092639e3c588d3d4b2e49006f3498976cfad5f
DaywisonSilva <daywisonsilvasou@gmail.com>
2022-05-14 18:56:17 -0300

    fisrt commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <q-card v-ripple>
+      <q-card-section
+        ><h1 class="text-h5">{{ title }}</h1></q-card-section
+      >
+      <q-separator></q-separator>
+      <q-card-section
+        >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
+        esse voluptate nemo quia aperiam sunt voluptatibus alias maiores
+        dolores? Tenetur, minus cupiditate officia consectetur delectus quo
+        eaque numquam voluptas sit!
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+<script setup lang="ts">
+const title = 'v-ripple test';
+</script>
